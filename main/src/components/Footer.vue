3f1c9c92bc2efe4cce8c1eb604a14d03cc8ddd3e
Tor Rydberg <crownroyalsuperslut@yahoo.com>
2025-09-10 21:51:22 -0400

    added footer content and styles

--- main/src/components/Footer.vue
+++ main/src/components/Footer.vue
@@ -5,16 +5,66 @@ import { ref } from 'vue'
 </script>
 
 <template>
-  <h1>footer</h1>
-
-  <div >
-    <p>This is where the footer will go</p>
+  <div class="footerCont">
+    <h3 class="info">Backstae Talks is a magazine
+      of casual, but in depth dialogues
+      on design and business. Our decisions
+      shape and influence this complex 
+      world-to have a chance to make the
+      right ones, we need to Talk<br/>
+      <span class="studioLink">&copy; 2021 <a
+        href="https://buromilk.com/"
+        >Published by studio milk</a></span><br/>
+        <a class="privacy"
+        href="https://buromilk.com/"
+        >Privacy Policy</a>
+    </h3>
+    <ul class="issueList">
+      <li>Issue #5</li>
+      <li>Issue #4</li>
+      <li>Issue #3</li>
+      <li>Issue #2</li>
+      <li>Issue #1</li>
+    </ul>
   </div>
 
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+a {
+  color: black;
+}
+.footerCont {
+  display: flex;
+  justify-content: space-between;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.info {
+  width: 20%;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+}
+
+.studioLink {
+  font-weight: 100;
+}
+
+.issueList {
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+}
+
+.issueList li {
+  font-size: 20px;
+  margin: 10px;
+}
+
+.privacy {
+ 
+ text-decoration: none;
 }
 </style>
